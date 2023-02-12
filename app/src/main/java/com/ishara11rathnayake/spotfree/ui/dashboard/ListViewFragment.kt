69b5afa4898956694d6c7cdd0b2e9929bb67ad01
ishara11rathnayake <ishara11rathnayake@gmail.com>
2023-02-13 03:21:02 +1100

    add list view fragment

--- app/src/main/java/com/ishara11rathnayake/spotfree/ui/dashboard/ListViewFragment.kt
+++ app/src/main/java/com/ishara11rathnayake/spotfree/ui/dashboard/ListViewFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.ishara11rathnayake.spotfree.databinding.FragmentDashboardBinding
+import com.ishara11rathnayake.spotfree.databinding.FragmentListBinding
 
-class DashboardFragment : Fragment() {
+class ListViewFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentListBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val listViewModel =
+            ViewModelProvider(this).get(ListViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentListBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
+        listViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
